0a64b0bd92730e33e3fd43304bea0d3b52313d4e
knallio <knallio@gmx.de>
2016-01-09 17:26:56 +0100

    implemented initial tcx class, api and functions follows as needed by rest

--- tcx.py
+++ tcx.py
@@ -1,42 +1,49 @@
 from lxml import etree
 
-GARMIN_URL="http://www.garmin.com/xmlschemas/"
-
-#TODO:
-#xsi:schemaLocation=GARMIN_URL+"TrainingCenterDatabase/v2 "+GARMIN_URL+"TrainingCenterDatabasev2.xsd"
-
-XML_NS="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
-XSI_NS="http://www.w3.org/2001/XMLSchema-instance"
-XSD_NS="http://www.w3.org/2001/XMLSchema"
-NS2_NS="http://www.garmin.com/xmlschemas/ActivityExtension/v2"
-
-NS_MAP={None: XML_NS, "xsi": XSI_NS, "xsd": XSD_NS , "ns2": NS2_NS}
+class tcx:
+	#Namespaces:
+	XML_NS="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
+	XSI_NS="http://www.w3.org/2001/XMLSchema-instance"
+	XSD_NS="http://www.w3.org/2001/XMLSchema"
+	NS2_NS="http://www.garmin.com/xmlschemas/ActivityExtension/v2"
+	#Schemalocation:
+	XML_SCHEMA="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd"
 
 
-tcx = etree.Element("TrainingCenterDatabase" , nsmap=NS_MAP)
-activities = etree.SubElement(tcx, "Activities")
+	def __init__(self):
+		NS_MAP={None: self.XML_NS, "xsi": self.XSI_NS, "xsd": self.XSD_NS , "ns2": self.NS2_NS}
+		self.tcx = etree.Element("TrainingCenterDatabase" , nsmap=NS_MAP)
+		self.tcx.set("{%s}schemaLocation" % self.XSI_NS, self.XML_SCHEMA)
 
-activity=etree.SubElement(activities, "Activity", Sport="Other")
-time=etree.SubElement(activity, "Id")
-time.text="2015"
+		self.activities = etree.SubElement(self.tcx, "Activities")
 
-lap=etree.SubElement(activity, "Lap", StartTime="2015")
+	def add_activity(self):
+		self.activity=etree.SubElement(self.activities, "Activity", Sport="Other")
+		time=etree.SubElement(self.activity, "Id")
+		time.text="2015"
 
-etree.SubElement(lap, "TotalTimeSeconds").text="5"
-etree.SubElement(lap, "DistanceMeters").text="100"
-etree.SubElement(lap, "MaximumSpeed").text="1.0"
-etree.SubElement(lap, "Calories").text="100"
-etree.SubElement(lap, "Intensity").text="Active"
-etree.SubElement(lap, "TriggerMethod").text="Manual"
-track=etree.SubElement(lap, "Track")
+		self.lap=etree.SubElement(self.activity, "Lap", StartTime="2015")
 
-etree.SubElement(track, "Trackpoint")
+		etree.SubElement(self.lap, "TotalTimeSeconds").text="5"
+		etree.SubElement(self.lap, "DistanceMeters").text="100"
+		etree.SubElement(self.lap, "MaximumSpeed").text="1.0"
+		etree.SubElement(self.lap, "Calories").text="100"
+		etree.SubElement(self.lap, "Intensity").text="Active"
+		etree.SubElement(self.lap, "TriggerMethod").text="Manual"
+		self.track=etree.SubElement(self.lap, "Track")
 
-tree = etree.ElementTree(tcx)
-tree.write("filename.xml",pretty_print=True, xml_declaration=True,encoding='utf-8')
+	def add_trackpoint(self):
+
+		etree.SubElement(self.track, "Trackpoint")
+
+	def write_xml(self,filename):
+		tree = etree.ElementTree(self.tcx)
+		tree.write(filename,pretty_print=True, xml_declaration=True,encoding='utf-8')
 
 
-#print(etree.tostring(root,pretty_print=True, xml_declaration=True))
 
+workout=tcx()
+workout.add_activity()
+workout.add_trackpoint()
 
-
+workout.write_xml("filename.xml")
